64a5cdf40d63f8761565233f66b8966793d4ef8a
sobhan mokhtari <sobhan22788@gmail.com>
2025-09-08 08:58:23 +0330

    accounts created

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.views import register, login
+
+
+app_name = 'accounts'
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+]
