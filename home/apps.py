900bda4ebe3b451d6769deb7936edf3c10621e65
sobhan mokhtari <sobhan22788@gmail.com>
2025-09-16 12:15:11 +0330

    test state dict deleted

--- home/apps.py
+++ home/apps.py
@@ -12,12 +12,7 @@ class HomeConfig(AppConfig):
     name = 'home'
 
     def ready(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        state_dict_path = os.path.join(current_dir, 'hovernet.pth')
-
         self.hovernet = HoverNet()
-        state_dict = torch.load(state_dict_path, map_location='cpu')
-        self.hovernet.load_state_dict(state_dict)
 
         print('model loaded')
 
